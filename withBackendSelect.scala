64375ccfa8fa74accafe5bb0c53e5d8c41dea7c1
shambaditya <69709105+shambaditya@users.noreply.github.com>
2025-04-02 01:20:53 -0400

    Add files via upload

---
+++ withBackendSelect.scala
@@ -0,0 +1,20 @@
+package proj_topK
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.{ConfigMap, BeforeAndAfterAllConfigMap}
+import firrtl.AnnotationSeq
+import chiseltest._
+
+abstract class TestWithBackendSelect
+    extends AnyFlatSpec
+    with BeforeAndAfterAllConfigMap {
+  var backend = ""
+  /***** simulate using tradle (default) *****/
+//  var simAnnos: AnnotationSeq = Seq()
+  /***** use verilator simulator *****/
+//   val simAnnos = Seq(VerilatorBackendAnnotation) ++ Seq(WriteVcdAnnotation)
+  /***** use VCS *****/
+   val simAnnos = Seq(VcsBackendAnnotation) ++ Seq(WriteVcdAnnotation)
+  /***** use icarus *****/
+  // val simAnnos = Seq(IcarusBackendAnnotation) ++ Seq(WriteVcdAnnotation)
+}
